81d133e77d7a3d0fa224f34853707bef6014d665
Victor Fontes Costa <victor@macbook-de-victor-fontes-costa.local>
2010-06-27 22:12:29 -0300

    fixing file encoding

--- django_mobile/decorators.py
+++ django_mobile/decorators.py
@@ -1,1 +1,45 @@
-#coding: utf-8  from django.shortcuts import render_to_response from django.template import RequestContext  try:     from functools import wraps except ImportError:      def wraps(wrapped, assigned=('__module__', '__name__', '__doc__'),               updated=('__dict__',)):         def inner(wrapper):             for attr in assigned:                 setattr(wrapper, attr, getattr(wrapped, attr))             for attr in updated:                 getattr(wrapper, attr).update(getattr(wrapped, attr, {}))             return wrapper         return inner   def render_to(template=None, mobile_template=None, mimetype="text/html"): 	""" 	This is a modified version of the decorator with the same name that can be found on django-annoying   	When is_mobile is True the template used to rendered is: 	M_TEMPLATE in returned dictionary, mobile_template decorator_parameter  	or TEMPLATE if there arent any mobile templates  	""" 	def renderer(function): 		@wraps(function) 		def wrapper(request, *args, **kwargs): 			output = function(request, *args, **kwargs) 			if not isinstance(output, dict): 				return output  			tmpl = output.pop('TEMPLATE', template) 			m_tmpl = output.pop('M_TEMPLATE', mobile_template)  			if request.is_mobile and m_tmpl: 				tmpl = m_tmpl			  			return render_to_response(tmpl, output, \                         context_instance=RequestContext(request), mimetype=mimetype) 		return wrapper 	return renderer
+#coding: utf-8
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+try:
+    from functools import wraps
+except ImportError: 
+    def wraps(wrapped, assigned=('__module__', '__name__', '__doc__'),
+              updated=('__dict__',)):
+        def inner(wrapper):
+            for attr in assigned:
+                setattr(wrapper, attr, getattr(wrapped, attr))
+            for attr in updated:
+                getattr(wrapper, attr).update(getattr(wrapped, attr, {}))
+            return wrapper
+        return inner
+
+
+def render_to(template=None, mobile_template=None, mimetype="text/html"):
+	"""
+	This is a modified version of the decorator with the same name that can be found on django-annoying 
+
+	When is_mobile is True the template used to rendered is:
+	M_TEMPLATE in returned dictionary, mobile_template decorator_parameter 
+	or TEMPLATE if there arent any mobile templates
+
+	"""
+	def renderer(function):
+		@wraps(function)
+		def wrapper(request, *args, **kwargs):
+			output = function(request, *args, **kwargs)
+			if not isinstance(output, dict):
+				return output
+
+			tmpl = output.pop('TEMPLATE', template)
+			m_tmpl = output.pop('M_TEMPLATE', mobile_template)
+
+			if request.is_mobile and m_tmpl:
+				tmpl = m_tmpl			
+
+			return render_to_response(tmpl, output, \
+                        context_instance=RequestContext(request), mimetype=mimetype)
+		return wrapper
+	return renderer
--- django_mobile/middleware.py
+++ django_mobile/middleware.py
@@ -1,1 +1,43 @@
-#coding: utf-8  from django.conf import settings import re  class MobileMiddleware(object):  	def is_mobile_useragent(self,request):  		if request.META.has_key('HTTP_USER_AGENT'): 			user_agent = request.META['HTTP_USER_AGENT']  			pattern = "(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|windows ce|pda|mobile|mini|palm|netfront)"  			if hasattr(settings, 'MOBILE_PATTERN'): 				pattern = settings.MOBILE_PATTERN  			prog = re.compile(pattern, re.IGNORECASE) 			match = prog.search(user_agent) 			return match  		return False  	def process_request(self, request):  		domain = request.META.get('HTTP_HOST', '').split('.')  		if 'm' in domain or 'mobile' in domain: 			request.is_mobile = True  		elif self.is_mobile_useragent(request): 			request.is_mobile = True 		else: 			request.is_mobile = False  		if request.is_mobile: 			pass 			#settings.TEMPLATE_DIRS = settings.MOBILE_TEMPLATE_DIRS 		else: 			pass 			#settings.TEMPLATE_DIRS = settings.DESKTOP_TEMPLATE_DIRS 			 		return None
+#coding: utf-8
+
+from django.conf import settings
+import re
+
+class MobileMiddleware(object):
+
+	def is_mobile_useragent(self,request):
+
+		if request.META.has_key('HTTP_USER_AGENT'):
+			user_agent = request.META['HTTP_USER_AGENT']
+
+			pattern = "(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|windows ce|pda|mobile|mini|palm|netfront)"
+
+			if hasattr(settings, 'MOBILE_PATTERN'):
+				pattern = settings.MOBILE_PATTERN
+
+			prog = re.compile(pattern, re.IGNORECASE)
+			match = prog.search(user_agent)
+			return match
+
+		return False
+
+	def process_request(self, request):
+
+		domain = request.META.get('HTTP_HOST', '').split('.')
+
+		if 'm' in domain or 'mobile' in domain:
+			request.is_mobile = True
+
+		elif self.is_mobile_useragent(request):
+			request.is_mobile = True
+		else:
+			request.is_mobile = False
+
+		if request.is_mobile:
+			pass
+			#settings.TEMPLATE_DIRS = settings.MOBILE_TEMPLATE_DIRS
+		else:
+			pass
+			#settings.TEMPLATE_DIRS = settings.DESKTOP_TEMPLATE_DIRS
+			
+		return None
